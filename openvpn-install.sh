d30a54f04766d6e4a833692dbb316cfd86c79c9a
raminsamadi123 <ramin.samadi+github@elev.ga.ntig.se>
2023-05-11 16:32:39 +0200

    fast git

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 ip_address="$(ip address | grep "inet" | grep "enp" | awk -F '/' '{print $1}' | awk '{print $2}')"
+ufw="/usr/bin/ufw"
 
 sudo apt-get update -y && sudo apt-get -y install ca-certificates wget net-tools gnupg
-
 wget https://as-repository.openvpn.net/as-repo-public.asc -qO /etc/apt/trusted.gpg.d/as-repository.asc
 
 echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/as-repository.asc] http://as-repository.openvpn.net/as/debian jammy main" >/etc/apt/sources.list.d/openvpn-as-repo.list
 
-sudo apt-get update -y && sudo apt-get -y install openvpn-as
+if [ -f "$ufw" ]; then
+	sudo ufw allow 443,80/tcp
+	sudo ufw allow 943,1194/udp
+fi
 
-echo -e "1. Type in Google Chrome: https://$ip_address:943/admin\n2. Install Openvpn Connect on Windows\n3. Type Client UI https://$ip_address:943/"
+sudo apt-get update -y && sudo apt-get -y install openvpn-as
+echo -e "To access the OpenVPN:\n1. Type in Google Chrome: https://$ip_address:943/admin\n2. Install Openvpn Connect on Windows\n3. Type Client UI https://$ip_address:943/"
