66c9f64c2973dbbd815cc91876fda9d2fd545820
raminsamadi123 <ramin.samadi+github@elev.ga.ntig.se>
2023-05-11 16:17:36 +0200

    fast git

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+ip_address="$(ip address | grep "inet" | grep "enp" | awk -F '/' '{print $1}' | awk '{print $2}')"
+
 sudo apt-get update -y && sudo apt-get -y install ca-certificates wget net-tools gnupg
 
 wget https://as-repository.openvpn.net/as-repo-public.asc -qO /etc/apt/trusted.gpg.d/as-repository.asc
@@ -8,6 +10,4 @@ echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/as-repository.asc] http:/
 
 sudo apt-get update -y && sudo apt-get -y install openvpn-as
 
-ip_address="$(ip address | grep "inet" | grep "enp" | awk -F '/' '{print $1}' | awk '{print $2}')"
-
-echo -e "1. Open Google Chrome\n2. Type in Google Chrome Admin UI: https://$ip_address:943/admin\n3. Install Openvpn Connect on Windows\n4. Type Client UI https://$ip_address:943/"
+echo -e "1. Type in Google Chrome: https://$ip_address:943/admin\n2. Install Openvpn Connect on Windows\n3. Type Client UI https://$ip_address:943/"
