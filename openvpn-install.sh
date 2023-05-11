769a4180b955d2206bbdd82a86b98ef76c81ea7b
raminsamadi123 <ramin.samadi+github@elev.ga.ntig.se>
2023-05-11 20:26:25 +0200

    fast git

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -20,4 +20,4 @@ username="$(grep "account with" "$HOME/openvpncredentials.txt" | awk '{ print $6
 password="$(grep "account with" "$HOME/openvpncredentials.txt" | awk '{ print $9 }' | sed 's/"//g')"
 
 clear
-echo -e "To access the OpenVPN:\n1. Type in Google Chrome: https://$ip_address:943/admin\n2. Login with\nusername: $username\npassword: $password\n3. Install Openvpn Connect on Windows\n4. Type Client UI https://$ip_address:943/"
+echo -e "To access the OpenVPN:\n1. Type in Google Chrome: https://$ip_address:943/admin\n\n2. Login with\nusername: $username\npassword: $password\n\n3. Install Openvpn Connect on Windows\n\n4. Type Client UI https://$ip_address:943/"
