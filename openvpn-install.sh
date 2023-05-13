4c0b6c99e6c486da206fec9db43e964458b00b78
raminsamadi123 <ramin.samadi+github@elev.ga.ntig.se>
2023-05-13 10:36:06 +0200

    fast git

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1,25 +1,39 @@
 #!/usr/bin/env bash
 
+# Script: OpenVPN Setup Script
+# Description: Sets up OpenVPN Access Server and provides instructions for accessing it
+# Prerequisites: Assumes administrative privileges and a Debian-based Linux distribution
+
+log_file="/var/log/openvpncredentials.log"
+client_connect_vpn_for_windows="https://openvpn.net/client-connect-vpn-for-windows/"
+
+# Update the package index files and install ca-certificates, wget, net-tools, gnupg
 sudo apt-get update -y && sudo apt-get -y install ca-certificates wget net-tools gnupg
 wget https://as-repository.openvpn.net/as-repo-public.asc -qO /etc/apt/trusted.gpg.d/as-repository.asc
 
+# Add OpenVPN repository to the sources list
 echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/as-repository.asc] http://as-repository.openvpn.net/as/debian jammy main" >/etc/apt/sources.list.d/openvpn-as-repo.list
 
+# If ufw exists, allow specific ports required by OpenVPN
 if command -v "ufw"; then
 	sudo ufw allow 443,80/tcp
 	sudo ufw allow 943,1194/udp
 fi
 
+# Update the package index files
 sudo apt-get update -y
-sudo apt-get -y install openvpn-as | sudo tee "$HOME/openvpncredentials.txt"
 
-admin_ui="$(grep "admin" "$HOME/openvpncredentials.txt" | awk '{ print $3 }')"
-client_ui="$(grep "Client" "$HOME/openvpncredentials.txt" | awk '{ print $3 }')"
+# Install OpenVPN Access Server and log installation output
+sudo apt-get -y install openvpn-as | sudo tee "$log_file"
 
-username="$(grep "account with" "$HOME/openvpncredentials.txt" | awk '{ print $6 }' | sed 's/"//g')"
-password="$(grep "account with" "$HOME/openvpncredentials.txt" | awk '{ print $9 }' | sed 's/"//g')"
+# Extract the URLs for the admin and client interfaces
+admin_ui="$(grep "admin" "$log_file" | awk '{ print $3 }')"
+client_ui="$(grep "Client" "$log_file" | awk '{ print $3 }')"
 
+# Extract the default OpenVPN credentials
+username="$(grep "account with" "$log_file" | awk '{ print $6 }' | sed 's/"//g')"
+password="$(grep "account with" "$log_file" | awk '{ print $9 }' | sed 's/"//g')"
+
+# Output instructions on how to set up OpenVPN and save it in a guide file
 clear
-echo -e "To access the OpenVPN:\n1. Type in Google Chrome: $admin_ui\n\n2. Login with\n	username: $username\n	password: $password\n\n3. Install OpenVPN Connect on Windows: https://openvpn.net/client-connect-vpn-for-windows/\n\n4. Type in OpenVPN Connect: $client_ui\n"
-
-echo -e "To access the OpenVPN:\n1. Type in Google Chrome: $admin_ui\n\n2. Login with\n	username: $username\n	password: $password\n\n3. Install OpenVPN Connect on Windows: https://openvpn.net/client-connect-vpn-for-windows/\n\n4. Type in OpenVPN Connect: $client_ui\n" >"/tmp/openvpn-guide"
+echo -e "To access the OpenVPN:\n1. Type in Google Chrome: $admin_ui\n\n2. Login with\n	username: $username\n	password: $password\n\n3. Install OpenVPN Connect on Windows: $client_connect_vpn_for_windows\n\n4. Type in OpenVPN Connect: $client_ui\n" | tee "/var/log/openvpn-guide.log"
